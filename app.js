0be078617165fe5cc90005944bb25c0dbfe7841d
rashmikaofficial98@gmail.com <rashmikaofficial98@gmail.com>
2021-07-12 17:09:25 +0530

    commit again 4

--- app.js
+++ app.js
@@ -73,6 +73,7 @@ app.post("/", function(req, res){
 // 
 app.listen(process.env.PORT || 3000, function(){
     console.log("This server is running in port 3000");
+    console.log(options);
 });
 
 
